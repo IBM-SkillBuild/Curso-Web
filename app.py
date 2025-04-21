f40caee41e3342a798a83dc23406c8939bd28e1b
IBM-SkillBuild <Api.Nasa.2022@gmail.com>
2025-04-22 01:47:31 +0200

    varios

--- app.py
+++ app.py
@@ -1028,5 +1028,5 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 10000))
+    port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
